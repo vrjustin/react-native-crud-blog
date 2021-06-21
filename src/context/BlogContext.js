3ada312b2fda052fcc7a0d9a5f8cd0cfe7ee8149
Justin Maronde <justin.maronde@cengage.com>
2021-06-21 07:05:44 -0400

    create a new hardcoded array of blogPosts for now that we can provide as the value to the Provider.
    refactor the name of value to blogPosts to represent the data in it better. Then render via a FlatList.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,15 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={15} >
+    const blogPosts = [
+        { title: 'Blog Post #1' },
+        { title: 'Blog Post #2' },
+        { title: 'Blog Post #3' },
+        { title: 'Blog Post #4' }
+    ];
+
+
+    return <BlogContext.Provider value={blogPosts} >
         {children}
     </BlogContext.Provider>
 };
