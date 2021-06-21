6717a24bf1552b0cc81eaaf958a111101cd5bae9
Justin Maronde <justin.maronde@cengage.com>
2021-06-21 07:18:09 -0400

    remove the hardcoded list of blogPosts. We will useState to update that array instead.
    instead of just passing down the blogPosts array as the value to the Provider, make it an object. First element in the object will continue to be the array of blogPosts, but now the second element will be a callback function. The call back function will invoke our helper function that calls that setState setter for setBlogPosts but just spreads the new entry from the existing blogPosts array. And creates a new one each time we call addBlogPost. There is nothing to call that function yet. That will be next.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    const blogPosts = [
-        { title: 'Blog Post #1' },
-        { title: 'Blog Post #2' },
-        { title: 'Blog Post #3' },
-        { title: 'Blog Post #4' }
-    ];
+    const [blogPosts, setBlogPosts] = useState([]);
 
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts], { title: `Blog Post #${blogPosts.length + 1}`});
+    };
 
-    return <BlogContext.Provider value={blogPosts} >
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost}} >
         {children}
     </BlogContext.Provider>
 };
