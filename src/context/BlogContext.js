67aa16b20988780098bb14c6aec26fad5bd2b51c
Justin Maronde <justin.maronde@cengage.com>
2021-06-23 13:44:47 -0400

    this was supposed to be in the previous commit as well.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -20,8 +20,9 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = dispatch => {
-    return (title, content) => {
-        dispatch({ type: 'add_blogpost', payload: { title: title, content: content} })
+    return (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: { title: title, content: content} });
+        callback();
     };
 };
 
