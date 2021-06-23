15a0b5b52e9bcf47390bf5c2e22be99b1e1df0a0
Justin Maronde <justin.maronde@cengage.com>
2021-06-23 14:55:38 -0400

    refactoring the common elements that were returned in JSX from both CreateScreen and EditScreen into a new BlogPostForm. All the state logic for the blogpost will be handled in the BlogPostForm as well.
    import the new BlogPostForm to both CreateScreen and EditScreen and return just that component now.

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,39 +1,15 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
-
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
     const { state } = useContext(Context);
     const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
 
-    const [title, setTitle] = useState(blogPost.title);
-    const [content, setContent] = useState(blogPost.content);
-
-    return (
-        <View>
-            <Text style={styles.label}>Edit Title:</Text>
-            <TextInput style={styles.input} value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
-            <Text style={styles.label}>Edit Content:</Text>
-            <TextInput style={styles.input} value={content} onChangeText={(newContent) => setContent(newContent)} />
-        </View>
-    )
+    return <BlogPostForm />
 };
 
-const styles = StyleSheet.create({
-    input: {
-        fontSize: 18,
-        borderWidth: 1,
-        borderColor: 'black',
-        marginBottom: 15,
-        padding: 5,
-        margin: 5
-    },
-    label: {
-        fontSize: 20,
-        marginBottom: 5,
-        marginLeft: 5
-    }
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
