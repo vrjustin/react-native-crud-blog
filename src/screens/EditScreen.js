cc8fe034c6c046671dadc84699578fa587648c5c
Justin Maronde <justin.maronde@cengage.com>
2021-06-23 14:36:16 -0400

    start making use of the Context provider and just like in the ShowScreen, pull the blogPost out of state using find. Use that existing blogPost to set the initial state of the title and content on the EditScreen.
    started to feel like an opportunity to refactor something into a more common component since ShowScreen and EditScreen are now having very similar code. That will be next...refactor.

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+
 
 const EditScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+
+    const [title, setTitle] = useState(blogPost.title);
+    const [content, setContent] = useState(blogPost.content);
+
     return (
         <View>
-            <Text>Edit Screen - {navigation.getParam('id')}</Text>
+            <Text style={styles.label}>Edit Title:</Text>
+            <TextInput style={styles.input} value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
+            <Text style={styles.label}>Edit Content:</Text>
+            <TextInput style={styles.input} value={content} onChangeText={(newContent) => setContent(newContent)} />
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5
+    }
+});
 
 export default EditScreen;
