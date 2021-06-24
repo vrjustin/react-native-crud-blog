613d1fdc4a038a08cdf4095b35a865e4dd2e3f5a
Justin Maronde <justin.maronde@cengage.com>
2021-06-24 06:50:05 -0400

    from the EditScreen will now pass down the onSubmit prop like we did for the CreateScreen when it calls BlogPostForm, but in addition to that we will also pass some initialValues for the form, that will come from the existing blogPost we are going to Edit.
    For now we will not handle the onSubmit from the editScreen other than console.log out the passed in title, content.
    
    In the BlogPostForm we will not destructure out the initialValues and use those to setState for title, content.
    
    ** Note that this breaks the functionality on the CreateScreen since we are NOT passing in initialValues from that call and so you get undefined is NOT an object as its looking into the initialValues.title. ** We will fix that next.

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -7,7 +7,12 @@ const EditScreen = ({ navigation }) => {
     const { state } = useContext(Context);
     const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
 
-    return <BlogPostForm />
+    return <BlogPostForm
+        initialValues={{ title: blogPost.title, content: blogPost.content }}
+        onSubmit={(title, content) => {
+            console.log(title, content);
+        }} 
+    />
 };
 
 const styles = StyleSheet.create({});
